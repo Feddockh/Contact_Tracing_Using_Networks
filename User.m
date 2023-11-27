2ae67de43e16b51007e5e68a8831e5fdf19607df
Hayden Feddock <hayden4feddock@gmail.com>
2023-11-27 02:21:51 -0500

    major updates to computation of contact, updates to the data generator function, created the coordinate calculation function, minor updates to other classes

--- User.m
+++ User.m
@@ -1,33 +1,46 @@
 classdef User
     properties (Access = private)
         ID
-        X
-        Y
+        Coordinates
         TX_db
         Status
     end
     methods
-        function obj = User(id, x, y, tx_db)
+        function obj = User(id, tx_db)
             if nargin > 0
                 obj.ID = id;
-                obj.X = x;
-                obj.Y = y;
                 obj.TX_db = tx_db;
+                obj.Coordinates = [];
             end
         end
         
         function id = getID(obj)
             id = obj.ID;
         end
-        function x = getX(obj)
-            x = obj.X;
-        end
-        function y = getY(obj)
-            y = obj.Y;
-        end
+
         function tx_db = getTX(obj)
             tx_db = obj.TX_db;
         end
+
+        % Add new coordinate to coordinate list
+        % TODO: Check if valid coordinate
+        function obj = addCoordinate(obj, x, y)
+            obj.Coordinates = [obj.Coordinates; x, y];
+        end
+
+        % Get the coordinate of the user
+        function x = getX(obj, t)
+            x = obj.Coordinates(t, 1);
+        end
+
+        function y = getY(obj, t)
+            y = obj.Coordinates(t, 2);
+        end
+
+        function coordinates = getCoordinates(obj)
+            coordinates = obj.Coordinates;
+        end
+
         function status = getStatus(obj)
             status = obj.Status;
         end
