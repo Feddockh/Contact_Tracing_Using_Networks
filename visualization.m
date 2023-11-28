e7692c2ac94117cc93a36fe1b4e1f337f72f2174
alexivensky <AWI4@pitt.edu>
2023-11-27 23:53:49 -0500

    Update visualization.m

--- visualization.m
+++ visualization.m
@@ -1,47 +1,34 @@
-clear
-clc
-
-% Generate the X and Y grid arrays using the MESHGRID function.
-r = zeros(50); % r will represent the input matrix. Current state is for demo purposes.
-
-% Integers for item representation
-person = 1;
-accessPoint = 2;
-rangeRadius = 3;
-
-sensitivity = -50;
-
-blue = 0.1
-
-% Define colormap for different objects
-colormap([1 1 1; 1 0 0; 0 1 0; 0 0 blue]); % Black, Red, Green, Blue
-
-AccessPoints = [];
-% also show users
-Users = [];
-
-imagesc(r);
-axis xy;
-
-% Update the plot in a loop
-for i = 1:100
-    % Generate new data (replace this with your data update logic)
-    newData = rand(10, 10);
+function[accessPoints] = visualization(dim)
+    % Generate the X and Y grid arrays using the MESHGRID function.
+    r = zeros(dim); % r will represent the input matrix. Current state is for demo purposes.
     
-    % Update the image data
-    [x,y] = ginput(1)
-    blue = blue + 0.1;
-    x = round(x)
-    y = round(y)
-
-    obj = AccessPoint(x, y, sensitivity);
-    AccessPoints{end+1} = obj
+    sensitivity = -50;
+    %rgb(173, 216, 230)
+    % Define colormap for different objects
+    colormap([1 1 1; 1 0 0; 0 1 0; 0 0 1]); % Black, Red, Green, Blue
     
-    r(y, x) = accessPoint;
+    accessPoints = {};
+    % also show users
+    Users = users;
     
-    
-    
-    % Force the figure to update
     imagesc(r);
     axis xy;
-end
+    
+    % Update the plot in a loop
+    for i = 1:100
+        % Update the image data
+        [x,y] = ginput(1);
+        x = round(x);
+        y = round(y);
+    
+        obj = AccessPoint(x, y, sensitivity);
+        accessPoints{end+1} = obj;
+        
+        r(y, x) = 3;
+        
+        
+        
+        % Force the figure to update
+        imagesc(r);
+        axis xy;
+    end
