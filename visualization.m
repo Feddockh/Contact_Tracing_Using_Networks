0a5efee39b12eccb22fbb61fb1973984a66e907f
alexivensky <AWI4@pitt.edu>
2023-11-28 17:28:52 -0500

    added numPoints

--- visualization.m
+++ visualization.m
@@ -1,20 +1,18 @@
-function[accessPoints] = visualization(dim)
+function[accessPoints] = visualization(dim, numPoints)
     % Generate the X and Y grid arrays using the MESHGRID function.
     r = zeros(dim); % r will represent the input matrix. Current state is for demo purposes.
     
     sensitivity = -50;
-    %rgb(173, 216, 230)
     % Define colormap for different objects
     colormap([1 1 1; 1 0 0; 0 1 0; 0 0 1]); % Black, Red, Green, Blue
     
     accessPoints = {};
-    Users = users; % NOTE: Not sure what this is for but this is giving me errors
     
     imagesc(r);
     axis xy;
     
     % Update the plot in a loop
-    for i = 1:100
+    for i = 1:numPoints
         % Update the image data
         [x,y] = ginput(1); % NOTE: Not sure how to exit the input window?
         x = round(x);
@@ -29,3 +27,4 @@ function[accessPoints] = visualization(dim)
         imagesc(r);
         axis xy;
     end
+
