456b9f2b1cd3ce237d3e9e0c22989ec6577b5eb1
alexivensky <AWI4@pitt.edu>
2023-11-28 21:13:09 -0500

    Update data_generator.m

--- data_generator.m
+++ data_generator.m
@@ -18,19 +18,19 @@ function [users] = data_generator(size, num_people, timesteps, testcase)
         end
 
     %% TESTCASE 2 %%
-    else if (testcase == 2)
+    elseif (testcase == 2)
         for (i = 1:timesteps)
-            if i <= size/2
-                users(1) = users(1).addCoordinate(i, 1); %  top left -> top minddle
-                users(2) = users(2).addCoordinate(1, size-i); % Bottom left -> left middle
-                users(3) = users(3).addCoordinate(size-i, size); %  bottom right ->bottle middle
-                users(4) = users(4).addCoordinate(size, i); % top right -> right middle
-            else
-                users(1) = users(1).addCoordinate(size/2, i-(size/2)+1); % top middle -> centre
-                users(2) = users(2).addCoordinate(i-(size/2)+1, size/2); % left middle -> centre
-                users(3) = users(3).addCoordinate(size/2, size-(i-(size/2))); % bottom middle -> centre
-                users(4) = users(4).addCoordinate(size-(i-(size/2)), size/2); % right middle -> centre
-            end
+            users(1) = users(1).addCoordinate(i, round(0.5*i)); % Bottom left -> top right
+            users(2) = users(2).addCoordinate(size - i, round(0.5*i));
+            users(3) = users(3).addCoordinate(i, size-i); % Top left -> bottom right
+            users(4) = users(4).addCoordinate(size-i, size-i); % Top right -> bottom left
+        end
+    elseif (testcase == 3)
+        for (i = 1:timesteps)
+            users(1) = users(1).addCoordinate(i, round(0.8*i)); % Bottom left -> top right
+            users(2) = users(2).addCoordinate(size - i, i);
+            users(3) = users(3).addCoordinate(i, round(0.3*(size-i))); % Top left -> bottom right
+            users(4) = users(4).addCoordinate(size-i, size-i); % Top right -> bottom left
         end
     end
 end
